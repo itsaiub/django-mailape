e7c6880209276f05b25ca687d5c50c860ff7a08a
itsaiub <itsaiub@gmail.com>
2019-09-08 16:04:19 +0600

    forms for mailinglist apps

---
+++ mailinglist/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from . import models
+
+
+class SubscriberForm(forms.ModelForm):
+    mailing_list = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=models.MailingList.objects.all(),
+        disabled=True
+    )
+
+    class Meta:
+        model = models.Subscriber
+        fields = ['mailing_list', 'email', ]
+
+
+class MessageForm(forms.ModelForm):
+    mailing_list = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=models.MailingList.objects.all(),
+        disabled=True
+    )
+
+    class Meta:
+        model = models.Message
+        fields = ['mailing_list', 'subject', 'body', ]
+
+
+class MailingListForm(forms.ModelForm):
+    owner = forms.ModelChoiceField(
+        widget=forms.HiddenInput,
+        queryset=get_user_model().objects.all(),
+        disabled=True
+    )
+
+    class Meta:
+        model = models.MailingList
+        fields = ['owner', 'name', ]
